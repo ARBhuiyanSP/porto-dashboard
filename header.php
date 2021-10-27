b1b1cc1d14927da70862dd8a31c75516520c90ed
unknown <arbhuiyan.pits@gmail.com>
2021-10-27 08:31:41 +0600

    first commit

---
+++ header.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html class="fixed sidebar-left-xs" data-style-switcher-options="{'sidebarSize': 'xs'}">
+	<head>
+		<!-- Basic -->
+		<meta charset="UTF-8">
+
+		<title>Global Soft Park | User Panel</title>
+		<meta name="keywords" content="Global Soft Park" />
+		<meta name="description" content="Global Soft Park - Comlete Online Solutions">
+		<meta name="author" content="Global Soft Park">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+		<!-- Web Fonts  -->
+		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css" />
+		<link rel="stylesheet" href="vendor/animate/animate.compat.css">
+
+		<link rel="stylesheet" href="vendor/font-awesome/css/all.min.css" />
+		<link rel="stylesheet" href="vendor/boxicons/css/boxicons.min.css" />
+		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.css" />
+		<link rel="stylesheet" href="vendor/bootstrap-datepicker/css/bootstrap-datepicker3.css" />
+
+		<!-- Specific Page Vendor CSS -->
+		<link rel="stylesheet" href="vendor/jquery-ui/jquery-ui.css" />
+		<link rel="stylesheet" href="vendor/jquery-ui/jquery-ui.theme.css" />
+		<link rel="stylesheet" href="vendor/bootstrap-multiselect/css/bootstrap-multiselect.css" />
+		<link rel="stylesheet" href="vendor/morris/morris.css" />
+
+		<!--(remove-empty-lines-end)-->
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="css/theme.css" />
+
+
+		<!--(remove-empty-lines-end)-->
+
+
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="css/custom.css">
+
+		<!-- Head Libs -->
+		<script src="vendor/modernizr/modernizr.js"></script>
+		<script src="master/style-switcher/style.switcher.localstorage.js"></script>
+
+	</head>
+<body>
